0ba4cb2b005b7ec18b614b15423fb90e2f89086c
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-08-13 21:30:46 +0900

    fix: classname of header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,7 @@
 export const Header = () => {
     return (
         <header className="p-4 bg-gray-800 text-white">
-            <h1 text-4xl font-bold text-left>
-                Keisan Dojo
-            </h1>
+            <h1 className="text-4xl font-bold text-left">Keisan Dojo</h1>
         </header>
     )
 }
