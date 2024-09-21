e20d869b55f98e4cff0e5d986552e7cf2f7ce745
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-09-21 21:55:13 +0900

    make scss variable imports easier

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import { NextConfig } from 'next'
+const path = require('path')
 
 const nextConfig: NextConfig = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'src', 'styles')],
+    },
     experimental: {
         reactCompiler: true,
     },
